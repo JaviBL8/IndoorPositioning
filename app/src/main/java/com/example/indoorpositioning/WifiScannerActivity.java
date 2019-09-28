a819f2a0c5bde574a4ff0921e194abe989f9923b
JaviBL8 <javibl8@correo.ugr.es>
2019-09-28 20:18:41 +0200

    Escaneo de redes con SSID - MAC - DISTANCIA

---
+++ app/src/main/java/com/example/indoorpositioning/WifiScannerActivity.java
@@ -0,0 +1,77 @@
+package com.example.indoorpositioning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.os.Bundle;
+import android.net.wifi.WifiManager;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WifiScannerActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
+    private ListView listView;
+    private Button buttonScan;
+    private List<ScanResult> results;
+    private ArrayList<String> availableNetworks = new ArrayList<>();
+    private ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonScan = findViewById(R.id.scanBtn);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanWifi();
+            }
+        });
+
+        listView = findViewById(R.id.wifiList);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if (!wifiManager.isWifiEnabled()) {
+            Toast.makeText(this, "Wifi is disabled ... You need to enable it", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, availableNetworks);
+        listView.setAdapter(adapter);
+    }
+    private void scanWifi() {
+        availableNetworks.clear();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        Toast.makeText(this, "Scanning Wifi", Toast.LENGTH_SHORT).show();
+        wifiManager.startScan();
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+
+            for (ScanResult scanResult : results){
+                availableNetworks.add(scanResult.SSID + " - " + scanResult.BSSID + " - " + (int)calculateDistance(scanResult.level,scanResult.frequency) + " m");
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+    public double calculateDistance(double levelInDb, double freqInMHz)    {
+        double exp = (27.55 - (20 * Math.log10(freqInMHz)) + Math.abs(levelInDb)) / 20.0;
+        return Math.pow(10.0, exp);
+    }
+}
