a490e45bf93bc5ee33d07cf9251a69f151138d5e
JaviBL8 <javibl8@correo.ugr.es>
2019-10-13 21:40:47 +0200

    Consulta a firebase y añado a la listview

--- app/src/main/java/com/example/indoorpositioning/WifiScannerActivity.java
+++ app/src/main/java/com/example/indoorpositioning/WifiScannerActivity.java
@@ -1,5 +1,6 @@
 package com.example.indoorpositioning;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.BroadcastReceiver;
@@ -9,14 +10,31 @@ import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.os.Bundle;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.core.Tag;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Document;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -28,6 +46,9 @@ public class WifiScannerActivity extends AppCompatActivity {
     private ArrayList<String> availableNetworks = new ArrayList<String>();
     private ArrayAdapter adapter;
     private Button scanBtn;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "WifiScannerActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,14 +72,15 @@ public class WifiScannerActivity extends AppCompatActivity {
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, availableNetworks);
         listView.setAdapter(adapter);
-/*
+
+        /*
         //Escanear wifi cada 10 segundos
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 scanWifi();
-            }
+            }Lugar
         },0,10000);
 */
     }
@@ -77,7 +99,8 @@ public class WifiScannerActivity extends AppCompatActivity {
 
             for (ScanResult scanResult : results){
                 String bssid = scanResult.BSSID.replace(":","");
-                availableNetworks.add(scanResult.SSID + " - " + bssid + " - " + (int)calculateDistance(scanResult.level,scanResult.frequency) + " m");
+                addAPs(bssid);
+                //availableNetworks.add(scanResult.SSID + " - " + bssid + " - " + (int)calculateDistance(scanResult.level,scanResult.frequency) + " m");
                 adapter.notifyDataSetChanged();
             }
         }
@@ -88,5 +111,28 @@ public class WifiScannerActivity extends AppCompatActivity {
         return Math.pow(10.0, exp);
     }
 
+    private void addAPs(final String bssid){
+        CollectionReference apRef = db.collection("lugares");
+
+        db.collection("lugares")
+                .whereEqualTo("mac", bssid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String valor = (String) document.getData().get("lugar");
+                                availableNetworks.add(valor);
+                                adapter.notifyDataSetChanged();
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+    }
 
 }
